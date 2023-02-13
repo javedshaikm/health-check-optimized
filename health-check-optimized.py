26b765c99adb396fb4576db28abe1fb1bd882660
javedshaikm <javed.s.m@hotmail.com>
2023-02-13 08:42:35 +0530

    Update health-check-optimized.py
    Added concurrent.futures to execute each iteration of the for loop in a separate thread.

--- health-check-optimized.py
+++ health-check-optimized.py
@@ -10,13 +10,13 @@ import re
 from io import StringIO
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
+import concurrent.futures
 
 username = 'cisco'
 password = 'cisco'
 
-interfaces = {'10.0.10.100':['Ethernet0/0','Ethernet1/0'],
-'10.0.10.101':['FastEthernet0/0','FastEthernet0/1']}
+interfaces = {'10.0.10.100': ['Ethernet0/0', 'Ethernet1/0'],
+              '10.0.10.101': ['FastEthernet0/0', 'FastEthernet0/1']}
 platform = 'cisco_ios'
 
 
@@ -27,7 +27,7 @@ def send_mail(status=''):
     msg['From'] = '@gmail.com'
     msg['To'] = '@hotmail.com'
     msg['Subject'] = "This is Health check"
-    msg.attach(MIMEText(status, 'plain'))   
+    msg.attach(MIMEText(status, 'plain'))
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(fromaddr, "")
@@ -35,40 +35,32 @@ def send_mail(status=''):
     server.sendmail(fromaddr, toaddr, text)
     server.quit()
     print("Sending email:\n{status}".format(status=status))
-    return
 
-def term_out(output = ''):
-    old_stdout = sys.stdout
-    output = StringIO()
-    sys.stdout = output
-    sys.stdout = old_stdout
-    return
 
-status_for_hosts = []
+def check_status(host, interfaces, status_list):
+    status_list = []
+    connect = ConnectHandler(device_type=platform, ip=host, username=username, password=password)
+    output = connect.send_command('terminal length 0', expect_string=r'#')
+    output = connect.send_command('enable', expect_string=r'#')
+    host_name = connect.send_command('show run | in hostname', expect_string=r'#')
+    interface_status = connect.send_command(f'show ip int brief', expect_string=r'#')
+    data = pd.read_fwf(StringIO(interface_status),  widths=[27, 16, 3, 7, 23, 8])
+    for index, row in data.iterrows():
+        if row[4] == 'administratively down' or row[4] == 'down':
+            status_list.append(f"\nInterface {row[0]} is down in {host_name}\n")
 
-for key, value in interfaces.items():
-    for items in value:
-        status_list = []
-        connect = ConnectHandler(device_type=platform, ip=key, username=username, password=password)
-        output = connect.send_command('terminal length 0', expect_string=r'#')
-        output = connect.send_command('enable',expect_string=r'#')
-        host_name = connect.send_command('show run | in hostname',expect_string=r'#')                       
-        interface_status = connect.send_command(f'show ip int brief',expect_string=r'#')
-        term_out(output = 'interface_result')
-        data = pd.read_fwf(StringIO(interface_status),  widths=[27, 16, 3, 7, 23, 8])
-        for index, row in data.iterrows():
-            if row[4] == 'administratively down' or row[4] == 'down':
-                status_list.append(f"\nInterface {row[0]} is down in {host_name}\n")
-       
-        bgp_status = connect.send_command('show ip bgp summary | be N',expect_string=r'#')
-        term_out(output = 'bgp_result')
-        bgp_data = pd.read_fwf(StringIO(bgp_status),  delim_whitespace=True, header=None)
-        for index, row in bgp_data.iterrows():
-            if row[9] == 'Down' or row[9] == 'Idle' or row[9] == 'Active':
-                status_list.append(f"\nNeighbor {row[0]} is down in {host_name}\n")    
-        
-    # Use join() instead of string concatenation
-    full_status = ''.join(status_list)
-    status_for_hosts.append(full_status)
+    bgp_status = connect.send_command('show ip bgp summary | be N', expect_string=r'#')
+    bgp_data = pd.read_fwf(StringIO(bgp_status), delim_whitespace=True, header=None)
+    for index, row in bgp_data.iterrows():
+        if row[9] == 'Down' or row[9] == 'Idle' or row[9] == 'Active':
+            status_list.append(f"\nNeighbor {row[0]} is down in {host_name}\n")
+
+    return status_list
+
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    futures = [executor.submit(check_status, host, interfaces, []) for host in interfaces]
+
+status_for_hosts = [f.result() for f in futures]
 
 send_mail(status='\n'.join(status_for_hosts))
